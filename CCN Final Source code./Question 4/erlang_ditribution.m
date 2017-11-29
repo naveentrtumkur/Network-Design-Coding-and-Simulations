11ff2b6d37bca9522b3b7b9e4ef3e0f6c8b13a5a
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2017-11-29 10:43:07 -0500

    Erlang distribution.
    Computation of erlang distribution_service time.

--- CCN Final Source code./Question 4/erlang_ditribution.m
+++ CCN Final Source code./Question 4/erlang_ditribution.m
@@ -1,17 +1,16 @@
 %Used the concept of inverse CDF to calculate Erlang random variable.
 
 %Input the number of exponential random numbers to be generated.
-
-prompt1 = '\nEnter the value of Mu:';
-Mu = input(prompt1);
-prompt2 = '\nEnter the value of k:';
-k = input(prompt2);
-prompt3 = '\nEnter the count of random numbers:';
-n = input(prompt3);
-[X] = MatLab1(n);
+function erl = erlang_ditribution(Mu,k)
+%prompt1 = '\nEnter the value of Mu:';
+%Mu = input(prompt1);
+%prompt2 = '\nEnter the value of k:';
+%k = input(prompt2);
+[X] = MatLab1(k);
 
 %create the random value and store it in array exprn
     ranerlang = sum((-1/(Mu*k))*log(1-X));
     %fprintf('Value number =%d Random value = %f ,exprnd value = %f',i,X(i),ranexp(i));
     
-disp(ranerlang);
+%disp(ranerlang);
+end
